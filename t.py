9b3e294d36168b9758c8e9f35de78fff49a03d51
Subhendra Basu <basu.subhendra@outlook.com>
2023-07-03 09:51:53 +0530

    code

--- t.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-import sys
-f=open("./pi.txt","r")
-print(f.read(141))
--- w.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-f=open("./sqrt2.txt","r")
-g=open("./_sqrt2_.txt","w")
-g.write(f.read(141))
-f.close()
-g.close()
